a5882b3ea188dec2dd3a4356c693d497eb6409fd
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-06-19 22:06:25 -0700

    Removed comments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-let timeOfTheDay = 'AM';
 let submitBtn = document.getElementById('submit');
 submitBtn.addEventListener('click', handleSubmit);
+
 let easternEl = document.getElementById('eastern');
 let pacificEl = document.getElementById('pacific');
 
@@ -17,17 +17,4 @@ function handleSubmit() {
   let easternTimeStr = pacificTime.toLocaleTimeString('en-US', { timeZone: 'America/New_York' })
   easternEl.textContent = easternTimeStr;
   pacificEl.textContent = pacificTimeStr;
-
 }
-
-// function updateTime() {
-  // setInterval(() => {
-  //   let easternTime = moment.tz("US/Eastern");
-  //   let pacificTime = easternTime.clone().tz("US/Pacific");
-  //   easternEl.textContent = easternTime.format('hh:mm A');
-  //   pacificEl.textContent = pacificTime.format('hh:mm A');
-  // }, 1000)
-// }
-
-// updateTime();
-
